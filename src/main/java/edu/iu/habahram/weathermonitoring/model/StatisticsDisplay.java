3bd5debdd4000f5d636e5de0f40202a578095534
connortroyrogers <connortroyrogers@gmail.com>
2024-02-05 08:45:13 -0500

    initial commit

--- src/main/java/edu/iu/habahram/weathermonitoring/model/StatisticsDisplay.java
+++ src/main/java/edu/iu/habahram/weathermonitoring/model/StatisticsDisplay.java
@@ -1,6 +1,15 @@
 package edu.iu.habahram.weathermonitoring.model;
 
 public class StatisticsDisplay implements Observer, DisplayElement {
+
+    private Subject weatherData;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public StatisticsDisplay(Subject weatherData){
+        this.weatherData = weatherData;
+    }
     @Override
     public void update(float temperature, float humidity, float pressure) {
 
